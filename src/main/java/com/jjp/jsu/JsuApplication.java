784735ca0192dc7074f2bea6f3d6abc2317f7bc2
JISUKIM <kimjs117@gmail.com>
2026-03-20 13:30:40 +0900

    first commit

---
+++ src/main/java/com/jjp/jsu/JsuApplication.java
@@ -0,0 +1,13 @@
+package com.jjp.jsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jjp/jsu/JsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjp.jsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
